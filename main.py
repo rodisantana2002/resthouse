0651055cbfc5ac639a4fc6d8ac7c31708db03a1a
Rodolfo <rodolfosantana@linux.localdomain>
2019-07-26 18:59:13 -0300

    dgfd

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from model.pessoa import pessoa
-from model.enum import *
+# from model.pessoa import pessoa
+# from model.enum import *
 
 
-pessoa = pessoa("rodolfo", "jose", tipo.Fisica, genero.Masculino)
+# pessoa = pessoa("rodolfo", "jose", tipo.Fisica, genero.Masculino)
 
-print(pessoa.__str__())
+# print(pessoa.__str__())
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
