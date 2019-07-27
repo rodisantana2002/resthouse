6b69357b0a007d544f1f7a0ff51718e52b975d9b
Rodolfo <rodolfosantana@linux.localdomain>
2019-07-27 00:08:41 -0300

    yi

--- app.py
+++ app.py
@@ -10,13 +10,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def app():
+def index():
     return '<h1>Hello World!</h1>'
-
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
