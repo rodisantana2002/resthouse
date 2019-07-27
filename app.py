25acbf4c43847b87930475f65a2ef8b94f452b5a
Rodolfo <rodolfosantana@linux.localdomain>
2019-07-27 00:26:57 -0300

    sd

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
     app.run()
