a64397a57bdccf6a4acaeb009d315abee158f15e
Rodolfo <rodolfosantana@linux.localdomain>
2019-07-27 19:33:57 -0300

    sd

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import os
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.cfg')
+# app.config.from_pyfile('config.cfg')
+# 'SQLALCHEMY_DATABASE_URI'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://slgbapiwlblkgw:8430dc6f67f50a7d333656b1aa792b7c3549052e51bc6a464e134fae63236b03@ec2-174-129-41-127.compute-1.amazonaws.com:5432/dfcitm8gug7cja'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
