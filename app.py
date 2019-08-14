c1c7d843713bf3da442311564ffb90b81439296f
Rodolfo <rodolfosantana@linux.localdomain>
2019-08-14 13:58:28 -0300

    df

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from model.usuario import User
 
 app = Flask(__name__, template_folder='templates')
-
+bootstrap = Bootstrap(app)
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
 
@@ -30,5 +30,4 @@ def user():
 if __name__ == '__main__':
     db.create_all()
     port = int(os.environ.get('PORT', 5000))
-    Bootstrap(app)
     app.run(host='0.0.0.0', port=port, debug=True)
