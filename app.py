16486b59f09ce9064607754c42887f902962fd64
Rodolfo <rodolfosantana@linux.localdomain>
2019-08-14 12:31:23 -0300

    sdf

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from model.usuario import Usuario
+from model.usuario import User
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -17,12 +17,12 @@ db = SQLAlchemy(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', users=Usuario.query.all())
+    return render_template('index.html', users=User.query.all())
 
 
 @app.route('/user', methods=['POST'])
 def user():
-    u = Usuario(request.form['name'], request.form['email'])
+    u = User(request.form['name'], request.form['email'])
     db.session.add(u)
     db.session.commit()
     return redirect(url_for('index'))
--- model/usuario.py
+++ model/usuario.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
 
-class Usuario(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
