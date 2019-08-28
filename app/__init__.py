91cc222a99ea42faa32ab565ee5faa60f3fe26e5
Rodolfo <rodolfosantana@linux.localdomain>
2019-08-28 14:00:50 -0300

    dfsdf

--- app/__init__.py
+++ app/__init__.py
@@ -24,10 +24,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USERNAME'] = os.environ.get(
-        'GMAIL_USER') or 'papersrs2002@gmail.com'
-    app.config['GMAIL_PASSWORD'] = os.environ.get(
-        'GMAIL_PASSWORD') or '12345Perkons'
+    app.config['MAIL_USERNAME'] = os.environ.get('GMAIL_USER') or 'papersrs2002@gmail.com'
+    app.config['MAIL_PASSWORD'] = os.environ.get('GMAIL_PASSWORD') or '12345Perkons'
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
     app.config['MAIL_DEFAULT_SENDER'] = 'papersrs2002@gmail.com'
