2f6475458ed3eca25bb55f50d272e0bba9259d6a
Rodolfo <rodolfosantana@linux.localdomain>
2019-08-28 00:33:14 -0300

    sdf

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def create_app():
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'papersrs2002@gmail.com'
     app.config['MAIL_PASSWORD'] = '12345Perkons'
-    app.config['MAIL_USE_TLS'] = false
+    app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     app.config['MAIL_DEFAULT_SENDER'] = 'papersrs2002@gmail.com'
 
